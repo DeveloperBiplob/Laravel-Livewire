d24a78225ce9fabaa40c4500b3c43ab2cccfe626
DeveloperBiplob <developerbiplob.bd@gmail.com>
2024-06-04 11:37:44 +0600

    Livewire install | Make a livewire component With Counter function

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <div class="d-flex flex-column justify-center align-items-center align-content-center">
+        <button class="btn btn-primary" wire:click="increment">+</button>
+        <h3>{{ $counter }}</h3>
+        <button class="btn btn-primary" wire:click="decrement">-</button>
+    </div>
+</div>
